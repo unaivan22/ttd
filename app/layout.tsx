72bfd14f81cbf3d8bafff4a735c564dc6d75c64c
unaivan <energeek@Energeeks-MacBook-Pro-2.local>
2024-02-13 21:39:12 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,16 +7,57 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 import Footer from "./footer";
 
+// export const metadata: Metadata = {
+//   title: "Draw Signature",
+//   description: "Draw Signature Online",
+// };
+
+const APP_NAME = "Draw Signature";
+const APP_DEFAULT_TITLE = "Draw Signature";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Draw Signature Online";
+
 export const metadata: Metadata = {
-  title: "Draw Signature",
-  description: "Draw Signature Online",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={inter.className}>
